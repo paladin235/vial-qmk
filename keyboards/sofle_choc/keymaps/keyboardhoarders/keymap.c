cdbc14925505b3e7303f987da8b1899c95d1d172
Daniel Pierce <dpierce@phdata.io>
2025-02-18 13:21:15 -0600

    Restore some previously reverted mappings

--- keyboards/sofle_choc/keymaps/keyboardhoarders/keymap.c
+++ keyboards/sofle_choc/keymaps/keyboardhoarders/keymap.c
@@ -179,17 +179,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |  =   |  -   |  (   |   )  |  `   |-------.    ,-------| Left | Down |  Up  | Right|  Del | Bspc |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * |Shift | Undo |  _   |  [   |   ]  |  ~   |-------|    |-------|      | LStr |      | LEnd |      | Shift|
+ * |Shift | Undo |  _   |  [   |   ]  |  ~   |-------|    |-------| mute |mprev | mply | mnxt |      | Shift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LCTR | LAlt | LGUI |Enter/| /  ESC  /       \ BKSP \  |Space/ | RGUI | RAlt | RCTR |
  *            |      |      |      |lower |/       /         \      \ |raise  |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
 [2] = LAYOUT(
-  _______, _______ , _______ , _______ , _______ , QK_MAGIC_TOGGLE_CTL_GUI,                           _______,  _______  , _______,  _______ ,  _______ ,_______,
-  _______, _______ ,  LSFT(KC_EQUAL),  LSFT(KC_LCBR),  LSFT(KC_RCBR), LSFT(KC_BSLS), _______, KC_PGUP,   KC_PGDN, _______, _______, _______,
-  _______, KC_EQL,  KC_MINS,  LSFT(KC_9),  LSFT(KC_0), KC_GRV,                       KC_LEFT,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
-  _______, _______ , LSFT(KC_MINS), KC_COPY, KC_LCBR, KC_RCBR,  LSFT(KC_GRV),       _______,  XXXXXXX, _______, XXXXXXX, _______,   XXXXXXX, _______,
+  _______, _______, _______, _______, _______, QK_MAGIC_TOGGLE_CTL_GUI,          _______,  _______  , RGB_MOD, RGB_RMOD , RGB_HUI , RGB_HUD,
+  _______, _______, LSFT(KC_EQUAL),  LSFT(KC_LCBR),  LSFT(KC_RCBR), LSFT(KC_BSLS), _______, KC_PGUP, KC_PGDN, _______, _______, _______,
+  _______, KC_EQL,  KC_MINS,  LSFT(KC_9),  LSFT(KC_0), KC_GRV,                       KC_LEFT,  KC_DOWN, KC_UP, KC_RGHT,  KC_DEL, KC_BSPC,
+  _______, _______, LSFT(KC_MINS), KC_LCBR, KC_RCBR,  LSFT(KC_GRV), _______,      KC_MUTE, KC_MPRV, KC_MPLY, KC_MPRV, _______,   XXXXXXX, _______,
                      _______, _______, _______, MO(3), _______,       _______, _______, _______, _______, _______
 ),
 /* Adjust
