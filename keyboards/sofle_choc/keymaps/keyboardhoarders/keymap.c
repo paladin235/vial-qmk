cdacdd48954922e380233433804759fc0adc64bd
Daniel Pierce <dpierce@phdata.io>
2025-02-18 12:48:28 -0600

    Add delete key to layer 1

--- keyboards/sofle_choc/keymaps/keyboardhoarders/keymap.c
+++ keyboards/sofle_choc/keymaps/keyboardhoarders/keymap.c
@@ -169,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
   _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
   _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,       _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
-                    _______, _______, _______, _______, _______,       _______, MO(3), _______, _______, _______
+                    _______, _______, _______, _______, _______,       KC_DEL, MO(3), _______, _______, _______
 ),
 /* RAISE
  * ,----------------------------------------.                    ,-----------------------------------------.
